5fc28859daf870b197bacb2574dd30ff7e7e1bfc
Atcm <augustot_cm@gmail.com>
2024-10-02 08:22:45 -0500

    Actualizar

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -7,7 +7,7 @@ export default function Detalle() {
         <div className="md:justify-center md:items-center grid md:grid-cols-3 sm:grid-cols-2 md:w-screen">
             <div className="md:mt-20 sm:ml-7 sm:mt-10 lg:ml-96  md:w-3/6">
               <figure className="flex">
-                  <img src="./public/images/photo1.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                  <img src="./images/photo1.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                   <label htmlFor="" className="[writing-mode:vertical-rl] md:text-lg sm:text-xs">PRODUCT OWNER</label>
               </figure>
                   <figcaption className="md:text-2xl sm:text-base font-bold ">
@@ -17,7 +17,7 @@ export default function Detalle() {
                 </div>
             <div className=" sm:ml-7  sm:mt-28 lg:ml-56 items-end md:w-3/6 md:h-1/6">
                 <figure className="flex">
-                  <img src="./public/images/photo2.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                  <img src="./images/photo2.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                   <label htmlFor="" className="[writing-mode:vertical-rl] md:text-lg sm:text-xs">PRODUCT OWNER</label>
               </figure>
                 <figcaption className="md:text-2xl sm:text-base font-bold">
@@ -26,7 +26,7 @@ export default function Detalle() {
             </div>
             <div className="sm:mt-10 sm:ml-7 lg:ml-14 md:mt-20 md:w-3/6">
               <figure className="flex">
-                  <img src="./public/images/photo3.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                  <img src="./images/photo3.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                   <label htmlFor="" className="[writing-mode:vertical-rl] md:text-lg sm:text-xs">PRODUCT OWNER</label>
               </figure>
                 <figcaption className="md:text-2xl sm:text-base font-bold ">
@@ -35,7 +35,7 @@ export default function Detalle() {
               </div>
             <div className="sm:ml-7 sm:mt-28 lg:ml-96  md:w-3/6">
               <figure className="flex">
-                    <img src="./public/images/photo4.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                    <img src="./images/photo4.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                     <label htmlFor="" className="[writing-mode:vertical-rl] md:text-lg sm:text-xs">PRODUCT OWNER</label>
                 </figure>
                 <figcaption className="md:text-2xl sm:text-base font-bold ">
@@ -44,7 +44,7 @@ export default function Detalle() {
             </div>
             <div className="sm:mt-10 sm:ml-7 lg:ml-56 md:h-1/6 md:w-3/6">
                 <figure className="flex">
-                  <img src="./public/images/photo5.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                  <img src="./images/photo5.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                   <label className="[writing-mode:vertical-rl] md:text-lg text-xs"  >DEVELOPER</label>
                 </figure>
                   <figcaption className="md:text-2xl sm:text-base font-bold ">
@@ -53,7 +53,7 @@ export default function Detalle() {
               </div>
             <div className="mt-10 sm:ml-7 sm:mt-28 lg:ml-14 md:w-3/6"> 
                 <figure className="flex">
-                  <img src="./public/images/photo6.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
+                  <img src="./images/photo6.png" alt="" className="sm:w-40 sm:h-48 md:w-56 md:h-72"/>
                   <label htmlFor="" className="[writing-mode:vertical-rl] md:text-lg sm:text-xs">DEVELOPER</label>
                 </figure>
                 <figcaption className="md:text-2xl sm:text-base font-bold flex-col">
